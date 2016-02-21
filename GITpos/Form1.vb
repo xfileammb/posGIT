9bc0f4e2d917f12693091dc3880b3ce891a34a01
Xfile ammb <xfileammb@hotmail.com>
2016-02-21 17:46:47 +0630

    Close Button Adding

---
+++ GITpos/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("hello world by git")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
